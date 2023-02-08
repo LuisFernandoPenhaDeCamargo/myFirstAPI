8d8a8149a4fba306ac2a8e481ad138fff1fd3160
Luis Fernando Penha de Camargo <luisfernandopenhadecamargo@gmail.com>
2023-02-08 18:28:58 -0300

    Adicionando o Nodemon, testando condicionais, trabalhando querys e bodys.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 import express from "express";
 
 export const app = express();
+app.use(express.json());
 
 app.get("/nomeDaRota", (request, response) => {
+  let nome = request.query.nome;
+  console.log(request.query);
+  console.log(request.body);
+  if (nome == "William") {
+    return response.status(200).json({ mensagem: "Meu tutor." });
+  }
   response
     .status(200)
     .json({ mensagem: "Resposta retornada ao usuário da API." });
 });
 
-app.get("/outraRota", function (request, response) {
+app.post("/outraRota", function (request, response) {
+  let mensagem = "Default.";
+  const { nome, senha } = request.body;
+  console.log(request.query);
+  console.log(request.body);
+  if (nome == "Lucas") {
+    return response.status(200).json({ mensagem });
+  }
   response.status(200).json({
     mensagem:
       "Resposta retornada ao usuário da API utilizando outra forma de se declarar uma função.",
